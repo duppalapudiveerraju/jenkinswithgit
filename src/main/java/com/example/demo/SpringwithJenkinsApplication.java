7ded2d027db6ea8c7eca99b16c30cdf1c9c0d0a3
veeru <duppalapudiveerraju@gmail.com>
2023-12-13 23:02:56 +0530

    logger

--- src/main/java/com/example/demo/SpringwithJenkinsApplication.java
+++ src/main/java/com/example/demo/SpringwithJenkinsApplication.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringwithJenkinsApplication {
 
+
+	 public static final Logger logger = LoggerFactory.getLogger(SpringwithJenkinsApplication.class); 
 	public static void main(String[] args) {
-		System.out.println("Hello jenkins");
+		logger.info("==========welcome to jenkins========");
 		SpringApplication.run(SpringwithJenkinsApplication.class, args);
+		
 	}
 
 }
